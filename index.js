ad35e81ae6c1004e0d9d2b58a1496f683cda2707
Marquet Reid <you@example.com>
2018-03-29 19:13:11 -0400

    deleted gitignore

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const resume = JSON.parse(fs.readFileSync('./resume.json'))
+const port = process.env.PORT || 4000
+
+// const general = jsonObj
+//     .filter(g => g.type === "general")
+    // .forEach(g => { 
+    //     console.log(`${g.name} ${g.phoneNumber} ${g.email}`)
+    //     return(`${g.name} ${g.phoneNumber} ${g.email}`)
+    // })
+
+// const test = JSON.parse(general)
+
+// const brand = jsonObj
+//     .filter(b => b.type === "brand-statement")
+
+// const project = jsonObj
+//     .filter(p => p.type === "project")
+
+app.get('/', (req, res) => {
+    res.send(resume)
+})
+
+app.listen(port, () => {
+    console.log('It\'s Lit!!! 🔥🔥🔥')
+})
